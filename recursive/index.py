b3f5eb10e4e0b4e6ef38864f95a35bfe54ee998e
unknown <matteospinelli10@gmail.com>
2023-02-28 23:29:26 +0100

    fix

--- recursive/index.py
+++ recursive/index.py
@@ -1,9 +1,16 @@
 # this is a simple case of a recursion algorithm. More sofisticated cases will be presented with quicksort 
 # the algorithm involves sum an array of n elements without using loops
+"""
+i=2 return 6
+i=1 return 10
+i=0 return 12
+"""
 
-arr = [2,4,6]
+arr = [i for i in range(1, 101)]
 
-def sum(x, i):
-    if i < len(arr) - 1:
-        sum(x, i + 1)
-    return x + arr[i]
+def sum(i):
+    if i == len(arr) - 1:
+        return arr[i]
+    return arr[i] + sum(i + 1)
+
+    
