e842700895e85f4de6c4537172ae2f9aa6d737b4
MatteoSpinelli <matteospinelli10@gmail.com>
2023-03-04 12:51:07 +0100

    add recursive examples

--- recursive/index.py
+++ recursive/index.py
@@ -1,16 +1,51 @@
 # this is a simple case of a recursion algorithm. More sofisticated cases will be presented with quicksort 
 # the algorithm involves sum an array of n elements without using loops
 """
-i=2 return 6
-i=1 return 10
-i=0 return 12
+list = [] return 0
+list = [3] return 3
+list = [2,3] return 5
+list = [1,2,3] return 6
 """
 
 arr = [i for i in range(1, 101)]
 
-def sum(i):
-    if i == len(arr) - 1:
-        return arr[i]
-    return arr[i] + sum(i + 1)
+def sum(list):
+    if list == []:
+        return 0
+    return sum(list[1:]) + list[0]
+
+
+"""
+list = [] return 0
+list = [3] return 1
+list = [2,3] return 2
+list = [1,2,3] return 3
+"""
+
+
+def countItems(list):
+    # count the elements of a list
+    if list == []:
+        return 0
+    return countItems(list[1:]) + 1
+
+
+"""
+list = [] return 0
+list = [3] if 0 > 3 return 0 else 3 (return 3)
+list = [2,3] if 3 > 2 return 3 else 2 (return 3)
+list = [1,2,3] if 3 > 1 return 3 else 1
+"""
+
+def findMax(list):
+    # return the maximum element in a list
+    if list == []:
+        return 0
+    el = findMax(list[1:])
+    if el > list[0]:
+        return el
+    return list[0]
+
+
 
     
