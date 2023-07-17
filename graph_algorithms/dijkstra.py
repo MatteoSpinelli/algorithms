8bdc14e432a6f1295aaf45f1aa94594d7d036ee1
MatteoSpinelli <matteospinelli10@gmail.com>
2023-07-17 11:29:20 +0200

    add dikkstra algorithm

--- graph_algorithms/dijkstra.py
+++ graph_algorithms/dijkstra.py
@@ -4,29 +4,52 @@
 #   2. figure out what it the cost to go from the cheapest node to its neighbour
 #   repeat
 
+import numpy as np
+
 graph = {
-    "start": { "A": 6, "B": 2 },
-    "A": { "FIN": 1 },
-    "B": { "A": 3, "FIN": 5 },
-    "FIN": {}
+    "book": [["lp", 5], ["poster", 0]],
+    "lp": [["guitar", 15], ["drums", 20]],
+    "poster": [["guitar", 30], ["drums", 35]],
+    "guitar": [["piano", 20]],
+    "drums": [["piano", 10]],
+    "piano": [],
 }
 
-costs = {}
+nc = {
+    "lp": np.inf,
+    "poster": np.inf,
+    "guitar": np.inf,
+    "drums": np.inf,
+    "piano": np.inf,
+}
 
-parents = {}
+parents = {
+    "lp": "book",
+    "poster": "book",
+    "guitar": "",
+    "drums": "",
+    "piano": "",
+}
 
-def find_cheapest(start):
-    pass
-        
-
-def update_cost():
-    pass
-
-def update_parent():
-    pass
-
-def dijkstra_algo():
-    pass
-
-if __name__ == "__main__":
-    dijkstra_algo()
+def main():
+    for node in graph.keys():
+        weights = graph[node]
+        if len(weights) == 0:
+            break
+        for w in weights:
+            if node == "book":
+                nc[w[0]] = w[1]
+                continue
+            updatedCost = w[1] + nc[node]
+            if updatedCost < nc[w[0]]:
+                nc[w[0]] = updatedCost
+                parents[w[0]] = node
+    
+    path = ["piano"]
+    target = "piano"
+    while target != "book":
+        for key, value in parents.items():
+            if key == target:
+                path.append(value)
+                target = value
+    return path
